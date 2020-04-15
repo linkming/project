b66de80f9791cd467bedafad57e7a5e7e3720568
孙旭明 <sunxuming@dataexa.com>
2020-04-15 16:25:43 +0800

    项目中引用plop

---
+++ src/layout/slideBar/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="slider-bar">
+   我是侧边栏
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'slider-bar',
+  data () {
+    return {
+
+    }
+  },
+  mounted () {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+  .slider-bar{
+    padding: 12px;
+  }
+</style>
